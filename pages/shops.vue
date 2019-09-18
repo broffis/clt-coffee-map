add265cae992906fc52ac10d9b80f81ae105b213
Brandon <broffis@redventures.com>
2019-09-18 18:39:06 -0400

    Started styling changes for desktop cards

--- pages/shops.vue
+++ pages/shops.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="container">
+  <div class="shop-page">
     <div>
       <h1 class="title">
         Charlotte Coffee Map
       </h1>
       <Filters />
-      <shop-card
-        v-for="shop in shops"
-        :key="shop.id"
-        :name="shop.name"
-        :roaster="shop.roaster"
-        :logo="shop.logo"
-        :street-address="shop.address"
-        :city="shop.city"
-        :state="shop.state"
-        :zip-code="shop.zipCode"
-        :review="shop.review"
-        :social-media-links="{
-          websiteUrl: shop.websiteUrl,
-          facebookLink: shop.facebookLink,
-          twitterLink: shop.twitterLink,
-          instagramLink: shop.instagramLink
-        }"
-      />
+      <div class="cards">
+        <shop-card
+          v-for="shop in shops"
+          :key="shop.id"
+          :name="shop.name"
+          :roaster="shop.roaster"
+          :logo="shop.logo"
+          :street-address="shop.address"
+          :city="shop.city"
+          :state="shop.state"
+          :zip-code="shop.zipCode"
+          :review="shop.review"
+          :social-media-links="{
+            websiteUrl: shop.websiteUrl,
+            facebookLink: shop.facebookLink,
+            twitterLink: shop.twitterLink,
+            instagramLink: shop.instagramLink
+          }"
+        />
+      </div><!-- /.cards -->
     </div>
   </div>
 </template>
@@ -45,35 +47,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 4rem;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
